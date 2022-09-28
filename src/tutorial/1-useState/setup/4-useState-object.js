8e6bc2b0f0b7d994be047dde0fd7fb484744c488
Francis Maina <sebastianfrancix@gmail.com>
2022-09-28 13:40:49 +0300

    useState with objects

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // const [person, setPerson] = useState({
+  //   name: "Francis",
+  //   age: 22,
+  //   message: "What a good day to code.",
+  // });
+  // console.log(person);
+
+  const [name, setName] = useState("Francis");
+  const [age, setAge] = useState(22);
+  const [message, setMessage] = useState("What a good day to code!");
+
+  //Change Message Function
+  const changeMessage = () => {
+    // setPerson({ ...person, message: "Keep grinding!" });
+    setMessage("Today is sunny");
+  };
+
+  return (
+    <>
+      <h3> {name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button type="button" className="btn" onClick={() => changeMessage()}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
