4dc262476c0da51caa61a615f7866a1b714822c3
Francis Maina <sebastianfrancix@gmail.com>
2022-09-29 16:38:17 +0300

    useEffect basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log("Use Effect rendered");
+    if (value >= 1) {
+      document.title = `New Messages ${value}`;
+    }
+  }, [value]);
+
+  console.log("Rendered outside the useEffect");
+  return (
+    <>
+      <h1>{value}</h1>
+      <button type="button" onClick={() => setValue(value + 1)}>
+        Increase Value
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
