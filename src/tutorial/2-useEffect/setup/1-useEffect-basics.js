fd3653c8c1624a33e1ac443394c7807d9a7e7ae6
Francis Maina <sebastianfrancix@gmail.com>
2022-10-14 01:34:20 +0300

    revisited the useEffect basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from "react";
-// by default runs after every re-render
-// cleanup function
-// second parameter
+
+// By default, useEffect runs after every re-render
+//cleanup function
+//second parameter
+
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
 
+  //we place a callback function inside our useEffect hook
   useEffect(() => {
-    console.log("Use Effect rendered");
+    console.log("Call useEffect");
     if (value >= 1) {
-      document.title = `New Messages ${value}`;
+      document.title = `New Messages (${value})`;
     }
   }, [value]);
+  console.log("render component");
 
-  console.log("Rendered outside the useEffect");
   return (
     <>
       <h1>{value}</h1>
-      <button type="button" onClick={() => setValue(value + 1)}>
-        Increase Value
+      <button type="button" className="btn" onClick={() => setValue(value + 1)}>
+        Increment Me
       </button>
     </>
   );
